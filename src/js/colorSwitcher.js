44673bd3b469f805a1b0b305e92abb1fe5536284
didukhroma <70459354+didukhroma@users.noreply.github.com>
2021-02-23 23:06:43 +0200

    task switch color complete

--- src/js/colorSwitcher.js
+++ src/js/colorSwitcher.js
@@ -1,2 +1,36 @@
 import colors from './dataBase';
 import { randomIntegerFromInterval } from './randomizer';
+
+const refs = {
+  bodyRef: document.querySelector('body'),
+  startBtnRef: document.querySelector('button[data-action="start"]'),
+  stopBtnRef: document.querySelector('button[data-action="stop"]'),
+};
+//------
+let timerId = null;
+//------
+const changeColor = array => {
+  let colorIndex = randomIntegerFromInterval(array.length - 1);
+  refs.bodyRef.style.backgroundColor = array[colorIndex];
+};
+//------
+const startHandler = () => {
+  timerId = setInterval(changeColor, 1000, colors);
+  refs.startBtnRef.removeEventListener('click', startHandler);
+};
+//------
+const stopHandler = () => {
+  clearInterval(timerId);
+  startChange();
+};
+//------
+const startChange = () =>
+  refs.startBtnRef.addEventListener('click', startHandler);
+//------
+const stopChange = () => {
+  refs.stopBtnRef.addEventListener('click', stopHandler);
+};
+//------
+startChange();
+stopChange();
+//------
